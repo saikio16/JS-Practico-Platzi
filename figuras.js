dd46d44e253dd1c228c216f9fec9497bf28c2d90
Ernesto Valencia <ernestovalenciaing@gmail.com>
2021-08-08 15:29:56 -0700

    Se agregaron las funciones para calcular triangulo y circulo. Se acomodo un poco el html para que se vea mejor, aun no se agrega CSS

--- figuras.js
+++ figuras.js
@@ -56,3 +56,43 @@ function calcularAreaCuadrado(){
   const area = areaCuadrado(value);
   alert(area);
 }
+
+// Triangulo
+function calcularPerimetroTriangulo(){
+  const input1 = document.getElementById("InputTriangulo1");
+  const value1 = parseFloat(input1.value);
+  
+  const input2 = document.getElementById("InputTriangulo2");
+  const value2 = parseFloat(input2.value);
+  
+  const input3 = document.getElementById("InputTriangulo3");
+  const value3 = parseFloat(input3.value);
+
+  const perimetro = perimetroTriangulo(value1, value2, value3);
+  alert(perimetro);
+}
+
+function calcularAreaTriangulo(){
+  const input = document.getElementById("InputCuadrado");
+  const value = input.value;
+
+  const area = areaCuadrado(value);
+  alert(area);
+}
+
+//Circulo
+function calcularPerimetroCirculo(){
+  const input = document.getElementById("InputCirculo");
+  const value = input.value;
+
+  const perimetro = perimetroCirculo(value);
+  alert(perimetro);
+}
+
+function calcularAreaCirculo(){
+  const input = document.getElementById("InputCirculo");
+  const value = input.value;
+
+  const area = areaCirculo(value);
+  alert(area);
+}
