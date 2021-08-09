bf1af19c64f25e383ca50bb65fd1f7972586c78c
Ernesto Valencia <ernestovalenciaing@gmail.com>
2021-08-08 23:22:31 -0700

    Taller 2 completado, se agregaron las funciones y el boton funciona correctamente

--- descuentos.js
+++ descuentos.js
@@ -9,6 +9,21 @@ function calcularPrecioConDescuento(precio, descuento){
 }
 
 
+function priceDiscount(){
+    const inputPrice = document.getElementById("InputPrice")
+    const priceValue = inputPrice.value;
+
+    const inputDiscount = document.getElementById("InputDiscount")
+    const discountValue = inputDiscount.value;
+    
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const resultP = document.getElementById("ResultP");
+    resultP.innerHTML = `El precio con descuento es: $${precioConDescuento}`
+}
+
+
+
 // console.log({
 //     precioOriginal,
 //     descuento,
